261d5eeb35d535208386a47e6c97699e1114180f
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-11 07:56:59 +0530

    Fix folder name to 'components'; not 'containers'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { Sidebar } from './containers/Sidebar';
-import { MessagesList } from './containers/MessagesList';
-import { AddMessage } from './containers/AddMessage';
+import { Sidebar } from './components/Sidebar';
+import { MessagesList } from './components/MessagesList';
+import { AddMessage } from './components/AddMessage';
 
 class App extends Component {
   render() {
