cdcdc4eab917ba95bbde462f9d66e5ec8e91eb4b
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-11 08:22:38 +0530

    Update the Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,1 +1,24 @@
-<aside id="sidebar">Users</aside>
+import React from 'react'
+import PropTypes from 'proptypes'
+
+const Sidebar = ({ users }) => {
+  return (
+    <aside id="sidebar" className="sidebar">
+      <ul>
+        users.map(user => {
+          <li key={user.id}>{user.name}</li>
+        })
+      </ul>
+    </aside>
+  )
+
+  Sidebar.PropTypes = {
+    users: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired
+      }).isRequired
+    ).isRequired
+  }
+
+export default Sidebar
