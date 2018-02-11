e352902b8d29addb5aead947389caca5b8131117
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-11 07:51:15 +0530

    Refactor messages.js to use ActionTypes

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,7 +1,9 @@
+import types from '../constants/ActionTypes'
+
 const messages = (state = [], action) => {
   switch(action.type) {
-    case 'ADD_MESSAGE':
-    case 'MESSAGE_RECEIVED':
+    case types.ADD_MESSAGE:
+    case types.MESSAGE_RECEIVED:
       return state.concat([{
         message: action.message,
         author: action.author,
