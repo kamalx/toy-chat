0d0814988acef94ac78b3589c87e1f7012286a59
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-11 07:46:35 +0530

    Reorganise and split into components (yet to define)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Sidebar } from './containers/Sidebar';
+import { MessagesList } from './containers/MessagesList';
+import { AddMessage } from './containers/AddMessage';
 
 class App extends Component {
   render() {
     return (
       <div className="App" id="container">
-        <aside id="sidebar">Users</aside>
+        <Sidebar/>
         <section id="main">
-          <section id="messages-list">Messages List</section>
-          <section id="new-message">New Message</section>
+          <MessagesList/>
+          <AddMessage/>
         </section>
       </div>
     );
