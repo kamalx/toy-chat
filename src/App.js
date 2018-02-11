d31b8c4c64d3fc5e34b857f6fc1f43332d0fdfd2
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-11 07:00:47 +0530

    Add basic containers for the chat app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" id="container">
+        <aside id="sidebar">Users</aside>
+        <section id="main">
+          <section id="messages-list">Messages List</section>
+          <section id="new-message">New Message</section>
+        </section>
       </div>
     );
   }
