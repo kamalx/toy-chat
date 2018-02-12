b6a32afbec606ac78ac32171c0496eb83be29ca1
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-12 23:05:09 +0530

    Cleanup Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import PropTypes from 'proptypes'
+import propTypes from 'prop-types'
 
-const Sidebar = ({ users }) => {
-  return (
-    <aside id="sidebar" className="sidebar">
-      <ul>
-        users.map(user => {
+const Sidebar = ({ users }) => (
+  <aside id="sidebar" className="sidebar">
+    <ul>
+      {users.map(user => {
+        return (
           <li key={user.id}>{user.name}</li>
-        })
-      </ul>
-    </aside>
-  )
+        )
+      })}
+    </ul>
+  </aside>
+)
 
-  Sidebar.PropTypes = {
-    users: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired
-      }).isRequired
-    ).isRequired
-  }
+Sidebar.propTypes = {
+  users: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number.isRequired,
+      name: propTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
 
 export default Sidebar
