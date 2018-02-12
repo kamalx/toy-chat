a8e0c798b61a8a75616770546a7949a8b5432187
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-13 02:08:36 +0530

    Reorganize, and import some saga related stuff
    ... that I don't understand yet... :(

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { createSagaMiddleware } from 'redux-saga'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { addUser } from './actions';
 import chat from './reducers';
 import { setupSocket } from './sockets'
+import handleNewMessage from './sagas'
+import username from './utils/name'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(chat)
 
