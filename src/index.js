5e471d21983f2c9860cd707a753e13239e70fada
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-13 06:17:55 +0530

    Squash some bugs

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { createSagaMiddleware } from 'redux-saga'
+import createSagaMiddleware from 'redux-saga'
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { addUser } from './actions';
 import reducers from './reducers';
-import { setupSocket } from './sockets'
+import setupSocket from './sockets'
 import handleNewMessage from './sagas'
 import username from './utils/name'
 
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
-  reducers
+  reducers,
   applyMiddleware(sagaMiddleware)
 )
 
-const socket = setupSocket(store.dispatch, username)
+// console.log('store in src/index', store)
+// console.log('store.dispatch in src/index', store.dispatch)
 
+const socket = setupSocket(store.dispatch, username)
+console.log("socket in src/index ", socket)
 sagaMiddleware.run(handleNewMessage, {socket, username})
 
 ReactDOM.render(
