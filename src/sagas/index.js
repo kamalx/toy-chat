e23e43fc34d30170c321a8c6ef32c3d118071490
Kamaleshwar Morjal <kamal@morjal.com>
2018-02-13 06:19:11 +0530

    Add missing export

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,7 +3,10 @@ import * as types from '../constants/ActionTypes'
 
 const handleNewMessage = function* handleNewMessage(params) {
   yield takeEvery(types.ADD_MESSAGE, (action) => {
-    action.author = params.username,
+    // console.log("params to handleNewMessage in sagas/", params)
+    action.author = params.username
     params.socket.send(JSON.stringify(action))
   })
 }
+
+export default handleNewMessage
